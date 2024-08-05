78841e6fc8618d0c5363140df7bf5a5c1b926c69
halt <halt@dtllc.io>
2024-08-05 17:47:28 -0400

    clean up the archive

--- download_garys_tools.sh
+++ download_garys_tools.sh
@@ -4,4 +4,6 @@ set -ex
 
 wget "https://www.garykessler.net/software/ais_tools_latest.zip"
 unzip ais_tools_latest.zip
-
+cp -R ais_tools_20240601_latest/* .
+rm -rf ais_tools_20240601_latest
+rm -rf __MACOSX
