2a56e66f8b79990b9525fa17955f750da7ed1fb3
h4l7 <nick.haltmeyer@liberas.com>
2024-08-10 08:29:27 -0700

    rename archive

--- download_garys_tools.sh
+++ download_garys_tools.sh
@@ -4,7 +4,7 @@ set -ex
 
 wget "https://www.garykessler.net/software/ais_tools_latest.zip"
 unzip ais_tools_latest.zip
-cp -R ais_tools_20240601_latest/* .
-rm -rf ais_tools_20240601_latest
+cp -R ais_tools_20240601_latest\ copy/* .
+rm -rf ais_tools_20240601_latest\ copy/
 rm -rf __MACOSX
 rm ais_tools_latest.zip
